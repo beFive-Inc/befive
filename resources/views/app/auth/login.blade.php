7f9ace06f58062bb8249ac4b9f9807d85a45ee7a
Anthony Hoens <anthony-hoens@hotmail.com>
2022-05-16 14:36:08 +0200

    Update auth view

--- resources/views/app/auth/login.blade.php
+++ resources/views/app/auth/login.blade.php
@@ -4,131 +4,113 @@
     </x-slot>
 
 
-
     <x-slot name="metaData">
 
     </x-slot>
 
 
-
     <x-slot name="content">
-        <div class="auth">
-            <div class="auth__linear_position">
-                <div class="auth__linear_gradient"></div>
-            </div>
-            <div class="auth__preview_card preview_card">
-                <article class="preview_card__item item">
-                    <h2 aria-level="2" role="heading" class="item__title">
-                        {{ __('Prévisualisation de votre carte') }}
-                    </h2>
+        <div class="auth__right_container">
+            <div class="auth__logo_container">
+                <img src="{{ asset('parts/logo/befive_logo_white_background.svg') }}" class="auth__logo"
+                     alt="Logo de befive">
 
-                    <section class="user_card">
-
-                    </section>
-                </article>
+                <p class="auth__slogan_phrase">
+                    {{ __('messages.slogan.phrase') }}
+                </p>
             </div>
 
-            <div class="auth__right_container">
-                <div class="auth__logo_container">
-                    <img src="{{ asset('parts/logo/befive_logo_white_background.svg') }}" class="auth__logo" alt="Logo de befive">
 
-                    <p class="auth__slogan_phrase">
-                        {{ __('messages.slogan.phrase') }}
-                    </p>
-                </div>
+            <div class="auth__form_container">
+                <div class="auth__form_carrousel_overflow">
+                    <div class="auth__form_carrousel">
+                        <form action="{{ route('login') }}"
+                              method="post"
+                              class="auth__auth_form auth_form"
+                              id="check">
+                            @csrf
+
+                            <p class="auth_form__title">
+                                {{ __('auth.sign-in.title') }}
+                            </p>
+
+                            <div class="auth_form__field_container">
+                                <div class="auth_form__mega_field">
+                                    <div>
+                                        <label for="email" title="{{ __('Veuillez inscrire votre adresse e-mail') }}">
+                                            {{ __('auth.field.email') }}
+                                        </label>
+                                    </div>
+                                    <div class="input_anim">
+                                        <input type="email"
+                                               id="email"
+                                               name="email"
+                                               value="{{ old('email') }}"
+                                               placeholder="{{ __('auth.field.email') }}">
+                                        <span></span>
+                                    </div>
+
+                                    @error('email')
+                                    <span class="error">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="auth_form__mega_field">
+                                    <div>
+                                        <label for="password"
+                                               title="{{ __('Veuillez inscrire votre mot de passe') }}">
+                                            {{ __('auth.field.password') }}
+                                        </label>
+                                    </div>
+
+                                    <div class="input_anim">
+                                        <input type="password"
+                                               id="password"
+                                               name="password"
+                                               placeholder="********">
+                                        <span></span>
+                                    </div>
+
+                                    @error('password')
+                                    <span class="error">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
 
 
-                <div class="auth__form_container">
-                    <div class="auth__form_carrousel_overflow">
-                        <div class="auth__form_carrousel">
-                            <form action="{{ route('login') }}"
-                                  method="post"
-                                  class="auth__auth_form auth_form"
-                                  id="check">
-                                @csrf
+                            <div class="auth_form__button_container">
+                                <input type="submit"
+                                       value="{{ __('auth.sign-in.btn') }}"
+                                       class="principal_button mb-625"
+                                       disabled>
 
-                                <p class="auth_form__title">
-                                    {{ __('auth.sign-in.title') }}
+                                <a href="{{ route('password.request') }}" class="forgot_link">
+                                    {!! __('passwords.forget') !!}
+                                </a>
+
+                                <hr class="my-1">
+
+                                <p>
+                                    {!!  __('passwords.not.account') !!}
                                 </p>
 
-                                <div class="auth_form__field_container">
-                                    <div class="auth_form__mega_field">
-                                        <div>
-                                            <label for="email" title="{{ __('Veuillez inscrire votre adresse e-mail') }}">
-                                                {{ __('auth.field.email') }}
-                                            </label>
-                                        </div>
-                                        <div class="input_anim">
-                                            <input type="email"
-                                                   id="email"
-                                                   name="email"
-                                                   value="{{ old('email') }}"
-                                                   placeholder="{{ __('auth.field.email') }}">
-                                            <span></span>
-                                        </div>
-
-                                        @error('email')
-                                            <span class="error">
-                                                {{ $message }}
-                                            </span>
-                                        @enderror
-                                    </div>
-
-                                    <div class="auth_form__mega_field">
-                                        <div>
-                                            <label for="password"
-                                                   title="{{ __('Veuillez inscrire votre mot de passe') }}">
-                                                {{ __('auth.field.password') }}
-                                            </label>
-                                        </div>
-
-                                        <div class="input_anim">
-                                            <input type="password"
-                                                   id="password"
-                                                   name="password"
-                                                   placeholder="********">
-                                            <span></span>
-                                        </div>
-
-                                        @error('password')
-                                            <span class="error">
-                                                {{ $message }}
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-
-
-                                <div class="auth_form__button_container">
-                                    <input type="submit"
-                                           value="{{ __('auth.sign-in.btn') }}"
-                                           class="principal_button mb-625"
-                                           disabled>
-
-                                    <a href="{{ route('password.request') }}" class="forgot_link">
-                                        {!! __('passwords.forget') !!}
-                                    </a>
-
-                                    <hr class="my-1">
-
-                                    <p>
-                                        {!!  __('passwords.not.account') !!}
-                                    </p>
-
-                                    <a href="{{ route('register') }}"
-                                       class="secondary_button mt-1">
-                                        {{ __('auth.sign-up.btn') }}
-                                    </a>
-                                </div>
-                            </form>
-                        </div>
+                                <a href="{{ route('register') }}"
+                                   class="secondary_button mt-1">
+                                    {{ __('auth.sign-up.btn') }}
+                                </a>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
+        </div>
 
         </div>
     </x-slot>
-
 
 
     <x-slot name="script">
--- resources/views/app/auth/register.blade.php
+++ resources/views/app/auth/register.blade.php
@@ -12,41 +12,128 @@
 
 
     <x-slot name="content">
-        <form action="{{ route('register') }}" method="post" id="check">
-            @csrf
+        <div class="auth__right_container">
+            <div class="auth__logo_container">
+                <img src="{{ asset('parts/logo/befive_logo_white_background.svg') }}" class="auth__logo"
+                     alt="Logo de befive">
 
-            <div>
-                <label for="pseudo" title="{{ __('Veuillez inscrire votre pseudo') }}">{{ __('Pseudo') }}</label>
-                <input type="name" id="pseudo" name="pseudo" placeholder="DoeJohn" value="{{ old('pseudo') }}">
-
-                @error('name')
-                    <span aria-errormessage="{{ $message }}">{{ $message }}</span>
-                @enderror
+                <p class="auth__slogan_phrase">
+                    {{ __('messages.slogan.phrase') }}
+                </p>
             </div>
 
-            <div>
-                <label for="email" title="{{ __('Veuillez inscrire votre adresse e-mail') }}">{{ __('Adresse e-mail') }}</label>
-                <input type="email" id="email" name="email" placeholder="john.doe@gmail.com" value="{{ old('email') }}">
 
-                @error('email')
-                    <span aria-errormessage="{{ $message }}">{{ $message }}</span>
-                @enderror
-            </div>
+            <div class="auth__form_container">
+                <div class="auth__form_carrousel_overflow">
+                    <div class="auth__form_carrousel">
+                        <form action="{{ route('register') }}"
+                              method="post"
+                              class="auth__auth_form auth_form"
+                              id="check">
+                            @csrf
 
-            <div>
-                <label for="password" title="{{ __('Veuillez inscrire votre mot de passe') }}">{{ __('Mot de passe') }}</label>
-                <input type="password" id="password" name="password" placeholder="********">
-                @error('password')
-                    <span aria-errormessage="{{ $message }}">{{ $message }}</span>
-                @enderror
-            </div>
+                            <p class="auth_form__title">
+                                {{ __('auth.sign-up.title') }}
+                            </p>
 
-            <div>
-                <button disabled title="{{ __('Se connecter') }}">
-                    {{ __('Se connecter') }}
-                </button>
+                            <div class="auth_form__field_container">
+                                <div class="auth_form__mega_field">
+                                    <div>
+                                        <label for="pseudo" title="{{ __('Veuillez inscrire votre pseudo') }}">
+                                            {{ __('Pseudo') }}
+                                        </label>
+                                    </div>
+                                    <div class="input_anim">
+                                        <input type="text"
+                                               id="pseudo"
+                                               name="pseudo"
+                                               value="{{ old('pseudo') }}"
+                                               placeholder="{{ __('Veuillez inscrire votre pseudo') }}">
+                                        <span></span>
+                                    </div>
+
+                                    @error('pseudo')
+                                        <span class="error">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="auth_form__mega_field">
+                                    <div>
+                                        <label for="email" title="{{ __('Veuillez inscrire votre adresse e-mail') }}">
+                                            {{ __('auth.field.email') }}
+                                        </label>
+                                    </div>
+                                    <div class="input_anim">
+                                        <input type="email"
+                                               id="email"
+                                               name="email"
+                                               value="{{ old('email') }}"
+                                               placeholder="{{ __('auth.field.email') }}">
+                                        <span></span>
+                                    </div>
+
+                                    @error('email')
+                                        <span class="error">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="auth_form__mega_field">
+                                    <div>
+                                        <label for="password"
+                                               title="{{ __('Veuillez inscrire votre mot de passe') }}">
+                                            {{ __('auth.field.password') }}
+                                        </label>
+                                    </div>
+
+                                    <div class="input_anim">
+                                        <input type="password"
+                                               id="password"
+                                               name="password"
+                                               placeholder="********">
+                                        <span></span>
+                                    </div>
+
+                                    @error('password')
+                                        <span class="error">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+
+                            <div class="auth_form__button_container">
+                                <input type="submit"
+                                       value="{{ __('auth.sign-up.btn') }}"
+                                       class="principal_button mb-625"
+                                       disabled>
+
+                                <a href="{{ route('password.request') }}" class="forgot_link">
+                                    {!! __('passwords.forget') !!}
+                                </a>
+
+                                <hr class="my-1">
+
+                                <p>
+                                    {!!  __('Already have an account&nbsp;?') !!}
+                                </p>
+
+                                <a href="{{ route('login') }}"
+                                   class="secondary_button mt-1">
+                                    {{ __('auth.sign-in.btn') }}
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </form>
+        </div>
+
+        </div>
     </x-slot>
 
 

c0f6e8385b8f0833556035c21a28c69230bfe086
Anthony Hoens <anthony-hoens@hotmail.com>
2022-06-16 16:38:49 +0200

    Test for seeder

--- database/seeders/UserImgSeeder.php
+++ database/seeders/UserImgSeeder.php
@@ -17,11 +17,11 @@ class UserImgSeeder extends Seeder
         $users = User::where('email', 'LIKE', '%befive-chat.com')
             ->get();
 
-        User::find(1)->addMedia(storage_path('app/public/user/profile_img-1.webp'))
+        User::find(1)->addMedia(asset('parts/user/profile_img-1.webp'))
             ->toMediaCollection('profile');
 
         foreach ($users as $user) {
-            $user->addMedia(storage_path('app/public/user/profile_img-' . $user->id . '.webp'))
+            $user->addMedia(asset('parts/user/profile_img-' . $user->id . '.webp'))
                 ->toMediaCollection('profile');
         }
     }

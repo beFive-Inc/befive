11f7fc45022d9b0ead0afcc674a02f0145c7e767
Anthony Hoens <anthony-hoens@hotmail.com>
2022-06-16 16:41:08 +0200

    Remove img seeder

--- database/seeders/UserImgSeeder.php
+++ database/seeders/UserImgSeeder.php
@@ -14,15 +14,6 @@ class UserImgSeeder extends Seeder
      */
     public function run()
     {
-        $users = User::where('email', 'LIKE', '%befive-chat.com')
-            ->get();
 
-        User::find(1)->addMedia(asset('parts/user/profile_img-1.webp'))
-            ->toMediaCollection('profile');
-
-        foreach ($users as $user) {
-            $user->addMedia(asset('parts/user/profile_img-' . $user->id . '.webp'))
-                ->toMediaCollection('profile');
-        }
     }
 }
